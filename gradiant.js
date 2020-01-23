7b6011ad1f3ffe84656a8af6fcbacfb41ad26210
akash@Ghostman004 <ghostmankotnala29@gmail.com>
2020-01-23 21:24:20 +0530

    some correction

--- gradiant.js
+++ gradiant.js
@@ -3,9 +3,7 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body =document.getElementById("gradiant");
 
-// console.log(css);
-// console.log(color1);
-// console.log(color2);
+
 
 function setgradient()
 {
